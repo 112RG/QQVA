96921b988673158300a8d00fa99b728977bc0464
Rylee George <ryleegeorge1@gmail.com>
2023-03-27 04:54:06 +0000

    Move home page to actual home /

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home, name="Home"),
+    path("", views.home, name="Home"),
     path("command/", views.command, name="Command"),
     path("help/", views.help, name="Help"),
     path("admin/", views.admin, name="Admin"),
