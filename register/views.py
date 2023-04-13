681378671c81bff6e8c89beb53df6fa3206f678a
PeterGregorczyk <93751415+PeterGregorczyk@users.noreply.github.com>
2023-04-13 04:45:06 +0000

    registration pt2 - update views.py

--- register/views.py
+++ register/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
 # Create your views here.
 def register (response):
-    form = UserCreationForm()
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = RegisterForm()
     return render(response, "register/register.html", {"form":form})
