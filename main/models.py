9b1506d9c276804b948561c09b7f81ec3ad1b3dc
Rylee George <ryleegeorge1@gmail.com>
2023-04-13 02:37:02 +0000

    Add user model

--- main/models.py
+++ main/models.py
@@ -1,19 +1,7 @@
 from django.db import models
 
-# Create your models here.
-
 class Users(models.Model):
-    user = models.CharField(max_length=200)
-
-#this is for testing purposes...
-
-    def __str__(self):
-        return self.user
-
-class Name(models.Model):
-    user = models.ForeignKey(Users, on_delete=models.CASCADE)
-    text = models.CharField(max_length=200)
-    complete = models.BooleanField()
-
-    def __str__(self):
-        return self.text
+    id = models.UUIDField(primary_key=True, null=False)
+    user_name = models.CharField(max_length=20, null=False)
+    reg_date = models.TimeField()
+    is_admin = models.BooleanField(default=False)
