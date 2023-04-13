33fdd5effa925082af3f288abf5d9115c9d88430
PeterGregorczyk <93751415+PeterGregorczyk@users.noreply.github.com>
2023-04-13 04:13:51 +0000

    registration update pt1

--- qqva/settings.py
+++ qqva/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "main.apps.MainConfig",
+    "register.apps.RegisterConfig",
 ]
 
 MIDDLEWARE = [
--- register/views.py
+++ register/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register (response):
+    form = UserCreationForm()
+    return render(response, "register/register.html", {"form":form})
