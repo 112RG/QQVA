cd8031b187cff9249d453d19454955a6ab0320e5
PeterGregorczyk <93751415+PeterGregorczyk@users.noreply.github.com>
2023-04-13 05:52:29 +0000

    registration pt4 - page working as intended

--- qqva/settings.py
+++ qqva/settings.py
@@ -41,9 +41,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "main.apps.MainConfig",
     "register.apps.RegisterConfig",
-    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -129,5 +130,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+
 #crispyforms layout
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
 CRISPY_TEMPLATE_PACK="bootstrap4"
