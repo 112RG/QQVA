3d47f2f3a59d7c362f44c3876df19f97cf46a40c
PeterGregorczyk <93751415+PeterGregorczyk@users.noreply.github.com>
2023-05-04 03:57:06 +0000

    adding all models

--- main/models.py
+++ main/models.py
@@ -5,3 +5,14 @@ class Users(models.Model):
     user_name = models.CharField(max_length=20, null=False)
     reg_date = models.TimeField()
     is_admin = models.BooleanField(default=False)
+
+class VoiceFingerprint(models.Model):
+    voice_id = models.UUIDField(primary_key=true, null=False)
+    id = models.ForeignKey("Users", on_delete=models.CASCADE)
+    #voice_fingerprint using BinaryField but probably needs to be fixed
+    voice_fingerprint = models.CharField(max_length=None)
+
+class Command(models.Model):
+    command_id = models.UUIDField(primary_key=true, null=False)
+    id = models.ForeignKey("Users", on_delete=models.CASCADE)
+    command_time = models.TimeField(auto_now=False, auto_now_add=False)
