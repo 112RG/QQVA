b1e4afa522883db80ed4fce2feb5120b9efa2669
PeterGregorczyk <93751415+PeterGregorczyk@users.noreply.github.com>
2023-05-04 04:05:42 +0000

    fixed models

--- main/models.py
+++ main/models.py
@@ -7,12 +7,12 @@ class Users(models.Model):
     is_admin = models.BooleanField(default=False)
 
 class VoiceFingerprint(models.Model):
-    voice_id = models.UUIDField(primary_key=true, null=False)
+    voice_id = models.UUIDField(primary_key=True, null=False)
     id = models.ForeignKey("Users", on_delete=models.CASCADE)
     #voice_fingerprint using BinaryField but probably needs to be fixed
-    voice_fingerprint = models.CharField(max_length=None)
+    voice_fingerprint = models.CharField(max_length=1000)
 
 class Command(models.Model):
-    command_id = models.UUIDField(primary_key=true, null=False)
+    command_id = models.UUIDField(primary_key=True, null=False)
     id = models.ForeignKey("Users", on_delete=models.CASCADE)
     command_time = models.TimeField(auto_now=False, auto_now_add=False)
