f5c1a35a0c8598f44fa5257275fac80d61bf7a9b
Rylee George <ryleegeorge1@gmail.com>
2023-06-09 16:31:28 +1000

    Update models to default uuid field and add command to Command field

--- main/models.py
+++ main/models.py
@@ -1,18 +1,19 @@
 from django.db import models
-
+import uuid
 class Users(models.Model):
-    id = models.UUIDField(primary_key=True, null=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_name = models.CharField(max_length=20, null=False, default="")
     reg_date = models.TimeField(null=True)
     is_admin = models.BooleanField(default=False)
 
 class VoiceFingerprint(models.Model):
-    voice_id = models.UUIDField(primary_key=True, null=False)
+    voice_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     id = models.ForeignKey("Users", on_delete=models.CASCADE)
     #voice_fingerprint using BinaryField but probably needs to be fixed
     voice_fingerprint = models.CharField(max_length=1000)
 
 class Command(models.Model):
-    command_id = models.UUIDField(primary_key=True, null=False)
-    id = models.ForeignKey("Users", on_delete=models.CASCADE)
-    command_time = models.TimeField(auto_now=False, auto_now_add=False)
+    command_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.ForeignKey("Users", on_delete=models.CASCADE, null=True)
+    command_time = models.TimeField(auto_now=False, auto_now_add=True)
+    command = models.CharField(max_length=20, null=False, default="")
