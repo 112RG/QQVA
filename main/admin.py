37a646fc9abb12338bc3079c0f03e00a47a38d5d
Rylee George <ryleegeorge1@gmail.com>
2023-06-09 16:32:21 +1000

    Register the command model

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Users
+from .models import Users, Command
 
 # Register your models here.
 
 admin.site.register(Users)
+admin.site.register(Command)
